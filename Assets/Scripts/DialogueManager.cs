1355c46e8a10fdbe47254ef639001636b44dcba4
Slaimon91 <slaimon91@gmail.com>
2020-04-26 22:55:37 +0200

    npc dialogue

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI dialogueText;
+    [SerializeField] GameObject dialogueBox;
+
+    private Queue<string> sentences;
+    bool dialogueStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        if(!dialogueStarted)
+        {
+            nameText.text = dialogue.name;
+
+            sentences.Clear();
+            dialogueStarted = true;
+            dialogueBox.SetActive(true);
+
+            foreach (string sentence in dialogue.sentences)
+            {
+                sentences.Enqueue(sentence);
+            }
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            dialogueStarted = false;
+            dialogueBox.SetActive(false);
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+    }
+}
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour, IInteractable
+{
+    public Dialogue dialogue;
+
+    public void Interact()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
