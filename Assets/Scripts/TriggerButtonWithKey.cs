7f108ab7a1148dfa2599f81f6cdfce9beb6e0083
Slaimon91 <slaimon91@gmail.com>
2020-05-10 00:36:53 +0200

    buttons

---
+++ Assets/Scripts/TriggerButtonWithKey.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class TriggerButtonWithKey : MonoBehaviour
+{
+    private bool holding = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CrossPlatformInputManager.GetButtonDown("Submit"))
+        {
+            holding = true;
+            GetComponent<HoldAssignButton>().HoldingButton();
+        }
+        else if (CrossPlatformInputManager.GetButtonUp("Submit"))
+        {
+            if(holding)
+            {
+                holding = false;
+                GetComponent<HoldAssignButton>().ReleasedButton();
+            }
+            
+        }
+    }
+}
