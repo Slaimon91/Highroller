15bfb1c3833dbd9693a5b48510ca1c838419e6ec
Slaimon91 <slaimon91@gmail.com>
2020-11-30 19:53:56 +0100

    .

---
+++ Assets/GaiablockFODBridge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GaiablockFODBridge : Gaiablockade
+{
+    [SerializeField] Tile leftBackBridgeTile;
+    [SerializeField] Tile leftFrontBridgeTile;
+    [SerializeField] Tile rightBackBridgeTile;
+    [SerializeField] Tile rightFrontBridgeTile;
+    [SerializeField] Tilemap colliderMap;
+    [SerializeField] Tilemap colliderInFrontMap;
+    [SerializeField] Vector3Int leftCell;
+    [SerializeField] Vector3Int rightCell;
+    public override void PaidGaiaBlock()
+    {
+        colliderMap.SetTile(leftCell, leftBackBridgeTile);
+        colliderMap.SetTile(rightCell, rightBackBridgeTile);
+        colliderInFrontMap.SetTile(leftCell, leftFrontBridgeTile);
+        colliderInFrontMap.SetTile(rightCell, rightFrontBridgeTile);
+    }
+}
